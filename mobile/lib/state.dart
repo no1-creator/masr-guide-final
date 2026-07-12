9c8d0d34817e042cdff3b9e4752fafb725147734
no1-creator <yasin.thabet88@gmail.com>
2026-07-12 06:15:16 +0300

    apk

--- mobile/lib/state.dart
+++ mobile/lib/state.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
-import 'package:flutter/foundation.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/foundation.dart' hide Category;
 import 'api.dart';
 import 'models.dart';
 
