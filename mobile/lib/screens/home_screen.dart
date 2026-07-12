78817a32adc5bebd061730c253ecbfcbbc1aaee6
no1-creator <yasin.thabet88@gmail.com>
2026-07-12 06:23:44 +0300

    Update home_screen.dart

--- mobile/lib/screens/home_screen.dart
+++ mobile/lib/screens/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Category;
 import 'package:provider/provider.dart';
 import '../state.dart';
 import '../models.dart';
